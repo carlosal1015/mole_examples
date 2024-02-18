a864c867d05b613799c044b8f4938cb495793ab7
Carlos Aznarán <caznaranl@uni.pe>
2024-02-18 13:06:28 -0500

    Update

---
+++ PKGBUILDs/README.md
@@ -0,0 +1,4 @@
+<!-- ```
+jupyter_contrib_core -> jupyter_nbextensions_configurator -> python-jupyter-matlab-proxy-bin
+python-matlab-proxy-bin -> python-jupyter-matlab-proxy-bin
+``` -->
---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+function verification() {
+  if hash conda 2>/dev/null; then
+    echo "You have conda installed."
+    active_environment
+  else
+    echo "Install miniconda3-bin or anaconda from AUR. For example:"
+    echo "yay -Syu miniconda3-bin"
+  fi
+}
+
+function active_environment() {
+  local major_version=3
+  local minor_version=11
+  echo "Activation environment start"
+  echo "By default we using python ${major_version}.${minor_version}"
+  # conda init zsh
+  # conda config --set auto_activate_base false
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/MATLAB/bin/glnxa64
+  # python -m venv --system-site-packages $PWD/.venv${major_version}${minor_version}
+  # source $PWD/.venv${major_version}${minor_version}/bin/activate
+  # pip install matlabengine==23.2.1
+  [ -d $PWD/.venv-${major_version}${minor_version} ] || conda env create -f environment${major_version}${minor_version}.yml --prefix $PWD/.venv${major_version}${minor_version}
+  conda activate $PWD/.venv${major_version}${minor_version} # matlab2023b
+}
+
+verification
