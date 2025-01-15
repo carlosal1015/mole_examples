d076178e5128e863238ddd43d17e393220a4859f
Carlos Aznarán <caznaranl@uni.pe>
2025-01-15 17:41:42 -0500

    Update

--- examples/octave/elliptic1D.m
+++ examples/octave/elliptic1D.m
@@ -1,8 +1,13 @@
 #!/usr/bin/env -S octave -qf
-% Solves the 1D Poisson's equation with Robin boundary conditions
-
-clc
-close all
+% Solves the 1D Poisson Equation with Robin Boundary Conditions and a
+% non-constant forcing right hand side using the Mimetic Method with
+% MOLE in Octave / MATLAB.
+%
+%     Δu = f
+%
+% u : Vertical Displacement of a membrane
+% f : Forcing right hand side
+% Δ : Laplace Operator
 
 addpath('/usr/share/mole/matlab/')
 
@@ -14,11 +19,16 @@ m = 2 * k + 1; % Minimum number of cells to attain the desired accuracy
 dx = (east - west) / m; % Step length
 
 L = lap(k, m, dx); % 1D Mimetic laplacian operator
+figure('visible', 'off');
+spy(L);
+saveas(gcf, "elliptic1Dsparsebefore.pdf", 'pdfcrop')
 
 % Impose Robin BC on laplacian operator
 a = 1;
 b = 1;
 L = L + robinBC(k, m, dx, a, b);
+spy(L);
+saveas(gcf, "elliptic1Dsparseafter.pdf", 'pdfcrop')
 
 % 1D Staggered grid
 grid = [west west + dx / 2:dx:east - dx / 2 east];
@@ -29,8 +39,7 @@ U(1) = 0; % West BC
 U(end) = 2 * exp(1); % East BC
 
 U = L \ U; % Solve a linear system of equations
-
-figure('visible', 'off');
+% save("-hdf5", "U")
 % Plot result
 plot(grid, U, 'o')
 hold on
@@ -39,4 +48,4 @@ legend('Approximated', 'Analytical', 'Location', 'NorthWest')
 title('Poisson''s equation with Robin BC')
 xlabel('x')
 ylabel('u(x)')
-saveas(gcf, "elliptic1D", 'pdf')
+saveas(gcf, "elliptic1D.pdf", 'pdfcrop')
