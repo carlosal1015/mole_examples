ad8322cf0e42bdaa5a5c09423b8ae4a56418bc11
Carlos Aznarán <caznaranl@uni.pe>
2025-03-02 16:40:12 -0500

    Update PKGBUILD

--- run.sh
+++ run.sh
@@ -13,16 +13,17 @@ function verification() {
 function active_environment() {
   local major_version=3
   local minor_version=13
+  local micro_version=2
   echo "Activation environment start"
-  echo "By default we using python ${major_version}.${minor_version}"
+  echo "By default we using python ${major_version}.${minor_version}.${micro_version}"
   # conda init zsh
   # conda config --set auto_activate_base false
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/MATLAB/bin/glnxa64
-  # python -m venv --system-site-packages $PWD/.venv${major_version}${minor_version}
-  # source $PWD/.venv${major_version}${minor_version}/bin/activate
+  # python -m venv --system-site-packages $PWD/.venv${major_version}${minor_version}${micro_version}
+  # source $PWD/.venv${major_version}${minor_version}${micro_version}/bin/activate
   # pip install matlabengine==24.2.2
-  [ -d $PWD/.venv-${major_version}${minor_version} ] || conda env create -f environment${major_version}${minor_version}.yml --prefix $PWD/.venv${major_version}${minor_version}
-  conda activate $PWD/.venv${major_version}${minor_version} # matlab2024b
+  [ -d $PWD/.venv-${major_version}${minor_version}${micro_version} ] || conda env create -f environment${major_version}${minor_version}${micro_version}.yml --prefix $PWD/.venv${major_version}${minor_version}${micro_version}
+  conda activate $PWD/.venv${major_version}${minor_version}${micro_version} # matlab2024b
 }
 
 verification
