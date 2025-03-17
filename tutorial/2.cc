5bace9fce330acee2d832de6bc0e314a89c3cddf
Carlos Aznarán <caznaranl@uni.pe>
2025-03-16 20:46:48 -0500

    Update

--- tutorial/2.cc
+++ tutorial/2.cc
@@ -1,87 +1,82 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 int main()
 {
+  //
+  // Example #1
+  //
 
-    //
-    // Example #1
-    //
+  // Create a vector<double> of length 0
+  std::vector<double> v1;
 
-    // Create a vector<double> of length 0
-    std::vector<double> v1;
+  // Extend the vector by appending a few values
+  v1.push_back(1.1);
+  v1.push_back(2.2);
+  v1.push_back(3.3);
 
-    // Extend the vector by appending a few values
-    v1.push_back(1.1);
-    v1.push_back(2.2);
-    v1.push_back(3.3);
+  // Read a value back using .at(index)
+  // and save it to a variable.
+  double v1_1 = v1.at(1);
 
-    // Read a value back using .at(index)
-    // and save it to a variable.
-    double v1_1 = v1.at(1);
+  // We can also read values using [index]
+  double v1_2 = v1[2];
 
-    // We can also read values using [index]
-    double v1_2 = v1[2];
+  // Get the current size of the vector
+  int v1_size = v1.size();
 
-    // Get the current size of the vector
-    int v1_size = v1.size();
+  // Let's print some info
+  std::cout << "v1: size: " << v1_size << std::endl;
+  std::cout << "v1: elements: " << v1.at(0) << ", " << v1.at(1) << ", "
+            << v1.at(2) << std::endl;
+  std::cout << "v1_1: " << v1_1 << std::endl;
+  std::cout << "v1_2: " << v1_2 << std::endl;
 
-    // Let's print some info
-    std::cout << "v1: size: " << v1_size << std::endl;
-    std::cout << "v1: elements: "
-              << v1.at(0) << ", " << v1.at(1) << ", " << v1.at(2)
-              << std::endl;
-    std::cout << "v1_1: " << v1_1 << std::endl;
-    std::cout << "v1_2: " << v1_2 << std::endl;
+  //
+  // Example #2
+  //
 
-    //
-    // Example #2
-    //
+  // Create a vector<int> of length 10
+  std::vector<int> v2(10);
 
-    // Create a vector<int> of length 10
-    std::vector<int> v2(10);
+  // Assign some values directly
+  v2[0] = 0; // Could also do v2.at(0) = 0
+  v2[1] = 1; // Could also do v2.at(1) = 1
 
-    // Assign some values directly
-    v2[0] = 0; // Could also do v2.at(0) = 0
-    v2[1] = 1; // Could also do v2.at(1) = 1
+  // Assign the rest of the elements using a loop.
+  // For fun, let's do a Fibonacci sequence.
+  for (size_t i = 2; i < v2.size(); i++) {
+    v2[i] = v2[i - 1] + v2[i - 2];
+  }
 
-    // Assign the rest of the elements using a loop.
-    // For fun, let's do a Fibonacci sequence.
-    for (size_t i = 2; i < v2.size(); i++)
-    {
-        v2[i] = v2[i - 1] + v2[i - 2];
-    }
+  // Let's print the content of v2 on a single line using a loop
+  std::cout << "v2:";
+  for (size_t i = 0; i < v2.size(); i++) {
+    std::cout << " " << v2[i];
+  }
+  std::cout << std::endl;
 
-    // Let's print the content of v2 on a single line using a loop
-    std::cout << "v2:";
-    for (size_t i = 0; i < v2.size(); i++)
-    {
-        std::cout << " " << v2[i];
-    }
-    std::cout << std::endl;
+  // Once again, now with an alternative way of looping
+  // through the values in a vector
+  std::cout << "v2: ";
+  for (int value : v2) {
+    std::cout << value << " ";
+  }
+  std::cout << std::endl;
 
-    // Once again, now with an alternative way of looping
-    // through the values in a vector
-    std::cout << "v2: ";
-    for (int value : v2)
-    {
-        std::cout << value << " ";
-    }
-    std::cout << std::endl;
+  //
+  // Example #3 -- other ways to initialize vectors
+  //
 
-    //
-    // Example #3 -- other ways to initialize vectors
-    //
+  // Create a vector<double> of length 5, with all
+  // elements intitialised to 1.0
+  std::vector<double> v3(5, 1.0);
 
-    // Create a vector<double> of length 5, with all
-    // elements intitialised to 1.0
-    std::vector<double> v3(5, 1.0);
+  // Create a vector<std::string> of length 3,
+  // with all elements initialised differently
+  std::vector<std::string> v4 = {"AAA", "BBB", "CCC"};
 
-    // Create a vector<std::string> of length 3,
-    // with all elements initialised differently
-    std::vector<std::string> v4 = {"AAA", "BBB", "CCC"};
-
-    // We're done
-    return 0;
+  // We're done
+  return 0;
 }
--- tutorial/3.cc
+++ tutorial/3.cc
@@ -4,63 +4,60 @@
 #include <iostream>
 #include <vector>
 
-void insertCoefficient(int id, int i, int j, double w, std::vector<Eigen::Triplet<double>> &coeffs,
+void insertCoefficient(int id, int i, int j, double w,
+                       std::vector<Eigen::Triplet<double>> &coeffs,
                        Eigen::VectorXd &b, const Eigen::VectorXd &boundary)
 {
-    int n = int(boundary.size());
-    int id1 = i + j * n;
+  int n = int(boundary.size());
+  int id1 = i + j * n;
 
-    if (i == -1 || i == n)
-        b(id) -= w * boundary(j); // constrained coefficient
-    else if (j == -1 || j == n)
-        b(id) -= w * boundary(i); // constrained coefficient
-    else
-        coeffs.push_back(Eigen::Triplet<double>(id, id1, w)); // unknown coefficient
+  if (i == -1 || i == n)
+    b(id) -= w * boundary(j); // constrained coefficient
+  else if (j == -1 || j == n)
+    b(id) -= w * boundary(i); // constrained coefficient
+  else
+    coeffs.push_back(Eigen::Triplet<double>(id, id1, w)); // unknown coefficient
 }
 
-void buildProblem(std::vector<Eigen::Triplet<double>> &coefficients, Eigen::VectorXd &b, int n)
+void buildProblem(std::vector<Eigen::Triplet<double>> &coefficients,
+                  Eigen::VectorXd &b, int n)
 {
-    b.setZero();
-    Eigen::ArrayXd boundary = Eigen::ArrayXd::LinSpaced(n, 0, M_PI).sin().pow(2);
-    for (int j = 0; j < n; ++j)
-    {
-        for (int i = 0; i < n; ++i)
-        {
-            int id = i + j * n;
-            insertCoefficient(id, i - 1, j, -1, coefficients, b, boundary);
-            insertCoefficient(id, i + 1, j, -1, coefficients, b, boundary);
-            insertCoefficient(id, i, j - 1, -1, coefficients, b, boundary);
-            insertCoefficient(id, i, j + 1, -1, coefficients, b, boundary);
-            insertCoefficient(id, i, j, 4, coefficients, b, boundary);
-        }
+  b.setZero();
+  Eigen::ArrayXd boundary = Eigen::ArrayXd::LinSpaced(n, 0, M_PI).sin().pow(2);
+  for (int j = 0; j < n; ++j) {
+    for (int i = 0; i < n; ++i) {
+      int id = i + j * n;
+      insertCoefficient(id, i - 1, j, -1, coefficients, b, boundary);
+      insertCoefficient(id, i + 1, j, -1, coefficients, b, boundary);
+      insertCoefficient(id, i, j - 1, -1, coefficients, b, boundary);
+      insertCoefficient(id, i, j + 1, -1, coefficients, b, boundary);
+      insertCoefficient(id, i, j, 4, coefficients, b, boundary);
     }
+  }
 }
 
 int main()
 {
-    std::cout << "Eigen version: "
-              << EIGEN_WORLD_VERSION
-              << "."
-              << EIGEN_MAJOR_VERSION
-              << "."
-              << EIGEN_MINOR_VERSION
-              << "\n";
+  std::cout << "Eigen version: " << EIGEN_WORLD_VERSION << "."
+            << EIGEN_MAJOR_VERSION << "." << EIGEN_MINOR_VERSION << "\n";
 
-    Eigen::MatrixXd M(2, 2);
-    std::cout << M << "\n";
+  Eigen::MatrixXd M(2, 2);
+  std::cout << M << "\n";
 
-    int n = 300; // size of the image
-    int m = n * n;
-    std::vector<Eigen::Triplet<double>> coefficients;
-    Eigen::VectorXd b(m);
-    buildProblem(coefficients, b, n);
+  int n = 300; // size of the image
+  int m = n * n;
+  std::vector<Eigen::Triplet<double>> coefficients;
+  Eigen::VectorXd b(m);
+  buildProblem(coefficients, b, n);
 
-    Eigen::SparseMatrix<double> A(m, m);
-    A.setFromTriplets(coefficients.begin(), coefficients.end());
-    Eigen::SimplicialCholesky<Eigen::SparseMatrix<double>> chol(A); // performs a Cholesky factorization of A
-    Eigen::VectorXd x = chol.solve(b);                              // use the factorization to solve for the given right hand side
-    // Eigen::SparseLU<Eigen::SparseMatrix<double>> solver;
-    // solver.compute(A);
+  Eigen::SparseMatrix<double> A(m, m);
+  A.setFromTriplets(coefficients.begin(), coefficients.end());
+  Eigen::SimplicialCholesky<Eigen::SparseMatrix<double>> chol(
+      A); // performs a Cholesky factorization of A
+  Eigen::VectorXd x = chol.solve(
+      b); // use the factorization to solve for the given right hand side
+  // Eigen::SparseLU<Eigen::SparseMatrix<double>> solver;
+  // solver.compute(A);
 
-    return 0;
+  return 0;
 }
